df4b59a0ab40596accf527c9bbed3577c8284b81
Adithya.S <86308022+adithya4@users.noreply.github.com>
2024-07-08 10:00:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def index():
     return file.read()
 
 @app.get("/translate")
-def translation(user_text : str=Form(...), languages: str=Form(...)):
-  translated_text = translator.translate(user_text, dest=languages)
+def translation(user_text : str=Form(...), language: str=Form(...)):
+  translated_text = translator.translate(user_text, dest=language)
   return translated_text.text
   
 
