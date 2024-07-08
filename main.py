ce02cef77b33ebbe173e06676784b1d39aaf00ce
Adithya.S <86308022+adithya4@users.noreply.github.com>
2024-07-08 08:41:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ app= FastAPI()
 
 translator = Translator()
 
+@app.get("/")
+def home():
+  return "WELCOME HOME"
+
 @app.get("/translate")
 def index():
   user_text = "apa kabar"
   languages = "en"
-  translated_text = translator.translate(user_text, dest="languages")
+  translated_text = translator.translate(user_text, dest=languages)
   return translated_text.text
   
 
