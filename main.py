0142368997787a94ea5b25a5b0f99a88a4925e24
Adithya.S <86308022+adithya4@users.noreply.github.com>
2024-07-08 11:11:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-from fastapi import FastAPI, Form 
+from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from googletrans import Translator
 
-app= FastAPI()
+app = FastAPI()
 
 translator = Translator()
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.get("/",response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def index():
-  with open("index.html",'r') as file:
-    return file.read()
+    with open("index.html", 'r') as file:
+        return file.read()
 
 @app.post("/translate")
-async def translation(user_text : str=Form(...), language: str=Form(...)):
-  translated_text = translator.translate(user_text, dest=language)
-  return {"translated_text" : translated_text.text}
-  
-
-  
-
+async def translation(user_text: str = Form(...), language: str = Form(...)):
+    translated_text = translator.translate(user_text, dest=language)
+    return {"translated_text": translated_text.text}
