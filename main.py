8483c666a620e0e5cd644905e5cfa056cc8bfa2c
Adithya.S <86308022+adithya4@users.noreply.github.com>
2024-07-08 08:53:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from googletrans import Translator
 
 app= FastAPI()
 
 translator = Translator()
 
-@app.get("/")
+@app.get("/",response_class=HTMLResponse)
 def home():
-  return "WELCOME HOME"
+  with open("index.html",'r') as file:
+    return file.read()
+
 
 @app.get("/translate")
 def index(user_text : str=Form(...), languages: str=Form(...)):
