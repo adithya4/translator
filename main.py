fd1a2468506f81bc09eae980ffac285fbc6fc50e
Adithya.S <86308022+adithya4@users.noreply.github.com>
2024-07-08 10:08:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form 
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from googletrans import Translator
 
 app= FastAPI()
 
 translator = Translator()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.get("/",response_class=HTMLResponse)
 def index():
